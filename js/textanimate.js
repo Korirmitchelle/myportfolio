ed1e5ff5590e8eb203e540d3b9670411f526bbb6
Korirmitchelle <korirmitchelle@gmail.com>
2016-10-07 11:19:10 +0300

    Animated text

---
+++ js/textanimate.js
@@ -0,0 +1,15 @@
+var animateText = function (target, message, index, interval) {
+    if (index < message.length) {
+
+        $(target).append(message[index++]);
+        /* keep adding letters till last one*/
+        setTimeout(function () {
+            animateText(target, message, index, interval);
+        }, interval);
+    }
+}
+
+
+$(function () {
+    animateText("#intromessage", "Hi,My name is Mitchelle. Welcome to my world!", 0, 500);
+});
