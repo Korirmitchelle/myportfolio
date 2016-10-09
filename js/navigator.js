b2bf171f080781a1bef89b962e2f81cf36b8ac50
Korirmitchelle <korirmitchelle@gmail.com>
2016-10-09 14:12:16 +0300

    introduction page

--- js/navigator.js
+++ js/navigator.js
@@ -1,13 +1,13 @@
+/*
 function switchVisible() {
-            if (document.getElementById('introduction')) {
+    if (document.getElementById('introduction')) {
 
-                if (document.getElementById('introduction').style.display == 'none') {
-                    document.getElementById('introduction').style.display = 'block';
-                    document.getElementById('mainsection').style.display = 'none';
-                }
-                else {
-                    document.getElementById('introduction').style.display = 'none';
-                    document.getElementById('mainsection').style.display = 'block';
-                }
-            }
-}
+        if (document.getElementById('introduction').style.display == 'none') {
+            document.getElementById('introduction').style.display = 'block';
+            document.getElementById('mainsection').style.display = 'none';
+        } else {
+            document.getElementById('introduction').style.display = 'none';
+            document.getElementById('mainsection').style.display = 'block';
+        }
+    }
+}*/
